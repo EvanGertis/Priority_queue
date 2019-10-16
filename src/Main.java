b509a944658a1dd9ca4c33d9539405d01219e8f6
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-10-16 19:24:55 -0400

    thus far

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,48 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		PriorityQueue<Integer> numbers = new PriorityQueue<>();
+		
+		numbers.add(750);
+		numbers.add(500);
+		numbers.add(900);
+		numbers.add(100);
+		
+		System.out.println(numbers.toString());
+		
+		while(!numbers.isEmpty())
+		{
+			System.out.println(numbers.remove());
+			System.out.println(numbers.toString());
+		}
+		
+		Comparator<String> stringLengthComparator = new Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+				return s1.length() - s2.length();
+			}
+		};
+		
+		System.out.println(stringLengthComparator.compare("Evan", "Tommmmmmmmm"));
+		
+		Comparator<String> stringLengthComparatorLambda = (s1, s2) -> {
+			return s1.length() - s2.length();
+		};
+		
+		System.out.println(stringLengthComparatorLambda.compare("Evan", "Tommmmmmmmm"));
+		
+		PriorityQueue<String> namePriorityQueue = new PriorityQueue<>(stringLengthComparator);
+		
+		namePriorityQueue.add("long");
+		namePriorityQueue.add("longer");
+		namePriorityQueue.add("longest");
+		
+		System.out.println(namePriorityQueue.toString());
+		
 	}
 
 }
